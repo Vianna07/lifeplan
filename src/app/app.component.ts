1015df77af0ebf9be3c2ce9ce743bd4c3af29667
Gustavo Vianna <viannagustavo71@gmail.com>
2024-03-10 16:35:30 -0300

    INITIAL-FEATURE-2: Estruturando projeto
    Criando styles para padronizar as telas, finalizando menu lateral e home
    page, implementando barra de scrool personalizada nas telas com certo
    nível de responsividade.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { BrowserStorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,50 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'lifeplan';
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  constructor(
+    // public browserStorageService: BrowserStorageService
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit(): void {
+    this.__getTheme();
+  }
+
+  ngAfterViewInit(): void {
+    this.onResize();
+    this.renderer.listen('window', 'resize', this.onResize.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    // window.removeEventListener('resize', this.onResize.bind(this));
+  }
+
+  @HostListener('window:resize')
+  private onResize() {
+    const pageId = this.elementRef.nativeElement.querySelector('#page')
+    const pageClass = this.elementRef.nativeElement.querySelector('.page');
+    const containerClass = this.elementRef.nativeElement.querySelector('.container');
+
+    if (pageId && pageClass && containerClass) {
+      const pageIdHeight = parseFloat(pageId.offsetHeight)
+      const pageClassHeight = parseFloat(pageClass.offsetHeight)
+
+      if (pageClassHeight > pageIdHeight) {
+        containerClass.style.maxHeight = `${pageIdHeight}px`
+      }
+    }
+  }
+
+  private __getTheme(): void {
+    // const theme = this.browserStorageService.get('theme');
+
+    // if (theme) {
+    //   document.body.classList.add(theme);
+    // } else {
+    //   document.body.classList.add('dark');
+    //   this.browserStorageService.set('theme', 'dark');
+    // }
+  }
 }
