b7af9156e5e41733ef145ad8eb0f47968fafac8a
lairap <lairaluizapires@gmail.com>
2024-03-13 11:37:52 -0300

    FEATURE-8: Adicionar tela para caso a pagina nao for encontrada
    Criado componente notfound para representar a tela quando a url nao for
    encontrada, adicionado componente rota coringa no app.routes.ts

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent {
+
+}
