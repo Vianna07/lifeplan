880685c999e9e5c2d3ebd70f788f6cdf188b44c8
Vianna07 <viannagustavo71@gmail.com>
2024-04-29 00:15:23 -0300

    BUG-FIX: Deixando prioridade do componente notfound por ultimo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,24 +5,20 @@ import { FinanceComponent } from './pages/finance/finance.component';
 import { InvestmentsComponent } from './pages/investments/investments.component';
 
 export const routes: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-    },
-    {
-        path: 'page-not-found',
-        component: NotFoundComponent,
-    },
-    {
-        path: '**',
-        redirectTo: 'page-not-found',
-    },
-    {
-        path: 'finance',
-        component: FinanceComponent,
-    },
-    {
-      path: 'finance/investments',
-      component: InvestmentsComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+      path: 'finance',
+      component: FinanceComponent,
+  },
+  {
+    path: 'finance/investments',
+    component: InvestmentsComponent
+  },
+  {
+      path: '**',
+      component: NotFoundComponent,
+  },
 ];
