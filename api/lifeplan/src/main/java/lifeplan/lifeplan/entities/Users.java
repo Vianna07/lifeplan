8b7f928ab9cb9371d87a15b106c17484a884f843
Vianna07 <viannagustavo71@gmail.com>
2024-05-19 20:13:47 -0300

    ADDITIONAL-FEATURE: Criando backend da aplicação
    Foi criado backend da aplicação, que será a api da aplicação, foi usado
    java spring e implementado docker

---
+++ api/lifeplan/src/main/java/lifeplan/lifeplan/entities/Users.java
@@ -0,0 +1,43 @@
+package lifeplan.lifeplan.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class Users {
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "locale")
+    private String locale;
+
+    @Column(name = "picture")
+    private String picture;
+}
---
+++ api/lifeplan/src/test/java/lifeplan/lifeplan/LifeplanApplicationTests.java
@@ -0,0 +1,13 @@
+package lifeplan.lifeplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
